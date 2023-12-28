e703a1cae051a2c81f6e4e0ef348ff84ed6461f2
Sourcetree\MURAM <sakurabaakito@kaede-creation.com>
2023-12-28 21:54:41 +0900

    Upload

---
+++ IP Checker/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace IP_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 11F);
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(1, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Global IP:";
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("メイリオ", 9F);
+            this.button1.ForeColor = System.Drawing.Color.Orange;
+            this.button1.Location = new System.Drawing.Point(5, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "取得する";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 11F);
+            this.label2.ForeColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(185, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Local IP:";
+            this.label2.DoubleClick += new System.EventHandler(this.label2_DoubleClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 98);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(396, 10);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(396, 108);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ IP Checker/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Sockets;
+
+namespace IP_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Global_IP = "";
+        string Local_IP = "";
+        private string hostname = "";
+
+        async void Get_IP()
+        {
+
+            var httpClient = new HttpClient();
+            Global_IP = await httpClient.GetStringAsync("https://api.ipify.org/");
+            label1.Text = "Global IP: " + Global_IP;
+            progressBar1.Value = 100;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            Global_IP = "";
+            Local_IP = "";
+
+
+            Get_IP();
+
+            progressBar1.Value = 50;
+
+            hostname = Dns.GetHostName();
+            IPAddress[] ips = Dns.GetHostAddresses(hostname);
+            foreach (IPAddress a in ips)
+            {
+                if (a.AddressFamily.Equals(AddressFamily.InterNetwork))
+                {
+                    Local_IP += a.ToString() + (Environment.NewLine);
+                    break;
+                }
+            }
+
+            progressBar1.Value = 80;
+
+            label2.Text = "Local IP: " + Local_IP;
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Global_IP);
+        }
+
+        private void label2_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Local_IP);
+        }
+    }
+}
